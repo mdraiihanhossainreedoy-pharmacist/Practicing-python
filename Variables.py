dd4225fa7bf22876406a6a72aca4eeb58b7ea254
Md. Raihan Hossain Redoy <mdraiihanhossainreedoy@gmail.com>
2026-06-02 15:12:56 +0600

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,6 @@
+#variables | strings
+name = "Md. Raihan Hossain Redoy"
+institute = "Gono Bishwabidyalay"
+session = "2022-2023"
+cgpa = "3.50"
+print("Assalamualaikum, I'm", name, "currently a student of", institute, "in the session of", session, "and my cgpa is", cgpa,"."  " Please wish me luck, i will greatfull to you.")
