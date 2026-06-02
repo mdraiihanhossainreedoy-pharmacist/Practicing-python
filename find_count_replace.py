ceeaf51b4e915cd9a3cab26d443ea90c746a2b4a
Md. Raihan Hossain Redoy <mdraiihanhossainreedoy@gmail.com>
2026-06-02 15:38:07 +0600

    Add files via upload

---
+++ find_count_replace.py
@@ -0,0 +1,11 @@
+
+#String | \n 
+print("I love to do python. \nbut i'm a lazy one.") #\n means next line
+
+
+#str.find | str.count | str.replace
+str1 = "I love to do python. \nbut i'm a lazy one."
+print(str1.find("love"))
+print(str1.count("l"))
+print(str1.count("o"))
+print(str1.replace("love", "hate"))
